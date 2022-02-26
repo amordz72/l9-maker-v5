58238992a7d013178d2b8d2a72c02229ebdd6883
amor <50753427+amordz72@users.noreply.github.com>
2022-02-26 22:38:46 +0100

    Post Grid

---
+++ app/Http/Livewire/Post/Grid.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Post;
+
+use Livewire\Component;
+
+class Grid extends Component
+{
+    public function render()
+    {
+        return view('livewire.post.grid');
+    }
+}
